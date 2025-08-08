c2635fdb814f76437dba9ecac177a7aab8eba363
MH Nipu <mazedul0191.nipu@gmail.com>
2025-08-08 17:23:28 +0600

    Fix Vercel deployment - add dynamic route handling and vercel.json config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'users/edit/:id',
+    renderMode: RenderMode.Dynamic
+  },
+  {
+    path: 'users/create',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'users',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
-import { Routes } from '@angular/router';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
+import { Routes } from "@angular/router";
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { UserFormComponent } from "./components/user-form/user-form.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: 'users', component: UserListComponent },
-  { path: 'users/create', component: UserFormComponent },
-  { path: 'users/edit/:id', component: UserFormComponent },
-  { path: '**', redirectTo: '/users' }
+  { path: "", redirectTo: "/users", pathMatch: "full" },
+  { path: "users", component: UserListComponent },
+  { path: "users/create", component: UserFormComponent },
+  { 
+    path: "users/edit/:id", 
+    component: UserFormComponent,
+    data: { renderMode: "dynamic" }
+  },
+  { path: "**", redirectTo: "/users" }
 ];
